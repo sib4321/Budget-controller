53a56181eeeadc7d1b6433796eb2530fe56762ce
Sergey Bubelo <sib4321@ukr.net>
2017-08-19 15:08:36 +0300

    Styles with architecture

---
+++ app.js
@@ -0,0 +1,31 @@
+var budgetController = (function () {
+    var x = 23;
+    var add = function (a) {
+        return x + a;
+    }
+
+    return {
+        publicTest: function (b) {
+            return add(b);
+        }
+    }
+
+})();
+
+var UIController = (function () {
+
+    //Some code
+
+})();
+
+
+var controller = (function (budgerCtrl, UICtrl) {
+
+    var z = budgerCtrl.publicTest(5);
+
+    return {
+        anotherPublic: function () {
+            console.log(z);
+        }
+    }
+})(budgetController, UIController);
